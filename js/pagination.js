744dbc366cad58bba43964acb75aceabb6146855
Cangkir Desa <66490512+cangkirdesa@users.noreply.github.com>
2022-08-04 21:03:16 +0700

    Update pagination.js

--- js/pagination.js
+++ js/pagination.js
@@ -3,7 +3,7 @@
 if(typeof InfiniteScroll !== 'undefined') { var infinite_scroll = new InfiniteScroll ({ type: 0,
 target: { posts: '.blogPts', post: '.ntry', anchors: '.blogPg', anchor: '.olLnk'},
 text: {
-load: '<a class='jsLd' expr:aria-label='data:messages.loadMorePosts' expr:data-text='data:messages.loadMorePosts' href='javascript:;'/>',
-loading: '<div class='jsLd wait nPst' expr:data-text='data:messages.loading'><svg viewBox='0 0 50 50' x='0px' y='0px'><path d='M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z'><animateTransform attributeName='transform' attributeType='xml' dur='0.6s' from='0 25 25' repeatCount='indefinite' to='360 25 25' type='rotate'/></path></svg></div>',
-loaded: '<div class='jsLd nPst' expr:data-text='data:messages.noResultsFound'/>',
-error: '<a class='jsLd error' expr:aria-label='data:messages.loadMorePosts' expr:data-text='data:messages.moreEllipsis' href='javascript:;'/>'} }); }
+load: '<a class="jsLd" expr:aria-label="data:messages.loadMorePosts" expr:data-text="data:messages.loadMorePosts" href="javascript:;"/>',
+loading: '<div class="jsLd wait nPst" expr:data-text="data:messages.loading"><svg viewBox='0 0 50 50' x='0px' y='0px'><path d='M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z'><animateTransform attributeName='transform' attributeType='xml' dur='0.6s' from='0 25 25' repeatCount='indefinite' to='360 25 25' type='rotate'/></path></svg></div>',
+loaded: '<div class="jsLd nPst" expr:data-text="data:messages.noResultsFound"/>',
+error: '<a class="jsLd error" expr:aria-label="data:messages.loadMorePosts" expr:data-text="data:messages.moreEllipsis" href="javascript:;"/>'} }); }
